383db3a90e284b339a2c0108f5424bdffcf4bf77
Assylkhan <asil_24_95@mail.ru>
2021-01-15 16:39:57 +0600

    comments added

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func main() {
 	sigs := make(chan os.Signal)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
+	// горутина для прослушивания сигналов
 	go func() {
 		sig := <-sigs
 		fmt.Println()
@@ -37,8 +38,7 @@ func main() {
 		done <- struct{}{}
 	}()
 
-	// initialize map and fill
-	m := make(map[int]int) // key - goroutine id : val - count of jobs done
+	m := make(map[int]int) // ключ - айди горутины : занчение - количество обработанных запросов
 	for gid := 0; gid < runtime.GOMAXPROCS(0); gid++ {
 		m[gid] = 0
 	}
@@ -48,7 +48,7 @@ func main() {
 
 	go func() {
 		for job := range doneJobsChan {
-			m[job.gid]++
+			m[job.gid]++ // обновляю стату по потокам
 			if job.err != nil {
 				fmt.Printf("%s\n", job.err.Error())
 			} else {
@@ -60,11 +60,36 @@ func main() {
 
 	<-done
 
+	// вывод общей статистики по потокам
 	for gid, cnt := range m {
 		fmt.Printf("%d: %d\n", gid, cnt)
 	}
 }
 
+// считывание урлов из стандартного входа
+func readUrls(reader *bufio.Reader) <-chan string {
+	urlsChan := make(chan string)
+
+	go func() {
+		for {
+			url, err := reader.ReadString('\n')
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				fmt.Printf("%s\n", err.Error())
+				continue
+			}
+			urlsChan <- strings.TrimSpace(url)
+		}
+
+		close(urlsChan)
+	}()
+
+	return urlsChan
+}
+
+// обработка входящих запросов
 func processJobs(urlsChan <-chan string) <-chan job {
 	doneJobsChan := make(chan job)
 
@@ -72,6 +97,7 @@ func processJobs(urlsChan <-chan string) <-chan job {
 		procsWg := &sync.WaitGroup{}
 		urlsWg := &sync.WaitGroup{}
 
+		// рапараллеливание обработки запросов по количеству вычеслительных ядер
 		for gid := 0; gid < runtime.GOMAXPROCS(0); gid++ {
 			procsWg.Add(1)
 
@@ -94,6 +120,7 @@ func processJobs(urlsChan <-chan string) <-chan job {
 	return doneJobsChan
 }
 
+// выполнение конртеного запроса в определенном потоке
 func fetchUrl(url string, gid int, doneJobs chan<- job, wg *sync.WaitGroup) {
 	defer wg.Done()
 
@@ -117,26 +144,4 @@ func fetchUrl(url string, gid int, doneJobs chan<- job, wg *sync.WaitGroup) {
 	doneJob.size = int64(len(body))
 
 	doneJobs <- doneJob
-}
-
-func readUrls(reader *bufio.Reader) <-chan string {
-	urlsChan := make(chan string)
-
-	go func() {
-		for {
-			url, err := reader.ReadString('\n')
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				fmt.Printf("%s\n", err.Error())
-				continue
-			}
-			urlsChan <- strings.TrimSpace(url)
-		}
-
-		close(urlsChan)
-	}()
-
-	return urlsChan
 }
